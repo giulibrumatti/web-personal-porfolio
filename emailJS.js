a9b5d16afd5bcf37ca7f55ef1fecd786084741fa
giulibrumatti <giulibrumatti97@gmail.coim>
2023-11-29 13:27:18 -0300

    Func form term

--- emailJS.js
+++ emailJS.js
@@ -1,21 +1,37 @@
 const btn = document.getElementById('button');
+const messAlert = document.getElementById('alert_id');
 
 document.getElementById('form')
- .addEventListener('submit', function(event) {
-   event.preventDefault();
+  .addEventListener('submit', function (event) {
 
-   btn.value = 'Enviando...';
+    event.preventDefault();
 
-   const serviceID = 'default_service';
-   const templateID = 'template_bf5mgbc';
+    const email = document.getElementById('email_id').value.trim();
 
-   emailjs.sendForm(serviceID, templateID, this)
-    .then(() => {
-        btn.value = 'Enviar';
-      alert('Correo enviado!');
+    btn.value = 'Enviando...';
+
+    const serviceID = 'default_service';
+    const templateID = 'template_bf5mgbc';
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (!emailRegex.test(email)) {
+      messAlert.textContent = 'Por favor, ingresa un correo electrónico válido.';
+      messAlert.style.color = 'red';
+      btn.value = 'Enviar';
       form.reset();
-    }, (err) => {
+    }else{
+    emailjs.sendForm(serviceID, templateID, this)
+      .then(() => {
         btn.value = 'Enviar';
-      alert(JSON.stringify(err));
-    });
-});
+        messAlert.textContent = 'Correo enviado!';
+        messAlert.style.color = 'green';
+        form.reset();
+      }) 
+      .catch((err) => {
+        messAlert.textContent = `Error: ${JSON.stringify(err)}`;
+        messAlert.style.color = 'red';
+        btn.value = 'Enviar';
+        form.reset();
+      });
+    }  
+  });
